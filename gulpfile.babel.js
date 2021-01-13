6e50c3dd9b5e12f4e5a8ccf342115bfd016a59de
andreszd <contretaa@gmail.com>
2021-01-13 11:53:05 -0400

    initial confing for use gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const sass = require('gulp-sass')
+const pug = require('gulp-pug')
+const { init, stream, reload } = require('browser-sync')
+
+
+/* definition tasks */
+gulp.task('babel', ()=>{
+    return gulp.src('./src/js/*.js') 
+            .pipe(babel())
+            .pipe(gulp.dest('./public/js'))
+})
+gulp.task('scss', ()=>{
+    return gulp.src('./src/scss/style.scss')
+            .pipe(sass({
+                outputStyle: 'compressed'
+            }))
+            .pipe(gulp.dest('./public/css'))
+            .pipe(stream())
+})
+gulp.task('pug', ()=>{
+    return gulp.src('./src/views/index.pug')
+            .pipe(pug())
+            .pipe(gulp.dest('./public'))
+})
+
+/* principal task */
+gulp.task('default', ()=>{
+    init({
+        server: './public'
+    })
+    gulp.watch('./src/views/**/*.pug', gulp.series('pug')).on('change', reload)
+    gulp.watch('./src/scss/**/*.scss', gulp.series('scss'))
+    gulp.watch('./src/js/*.js', gulp.series('babel')).on('change', reload)
+})
+
