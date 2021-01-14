96008269b5ba41312a5502b755631f626c24e189
andreszd <contretaa@gmail.com>
2021-01-14 11:21:59 -0400

    mobile menu implemented

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
-console.log('hello world')
-alert('ola')
+const mainNav = document.querySelector('#main-nav')
+const topBar = document.querySelector('#top-bar')
+const iconMenu = document.querySelector('#icon-menu')
+
+iconMenu.addEventListener('click', evt =>{
+    if(mainNav.classList.contains('main-nav--show')){
+        iconMenu.src = '/assets/images/icon-hamburger.svg'      
+    }else{
+        iconMenu.src = '/assets/images/icon-close.svg'      
+    }
+    topBar.classList.toggle('top-bar--show')
+    mainNav.classList.toggle('main-nav--show')
+})
