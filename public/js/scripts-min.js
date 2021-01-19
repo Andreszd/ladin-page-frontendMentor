f2426d74821f024481e7c56308630fb8df52f659
andreszd <contretaa@gmail.com>
2021-01-19 17:12:22 -0400

    finished accordion section

--- public/js/scripts-min.js
+++ public/js/scripts-min.js
@@ -21,6 +21,73 @@ var tabsItem = _toConsumableArray(document.querySelectorAll('.tabs__item'));
 
 var tabsContent = _toConsumableArray(document.querySelectorAll('.tabs__content'));
 
+var accordion = document.querySelector('#accordion');
+
+var accordionItems = _toConsumableArray(document.querySelectorAll('.accordion__item'));
+/* FUNCTIONS */
+
+
+function addClassActive(element, idx) {
+  if (element.nodeName === 'LI') element.classList.add('tabs__item--active');
+  tabsContent[idx].classList.add('tabs__content--active');
+}
+
+function removeClassItems(items) {
+  items.forEach(function (item, idx) {
+    if (item.nodeName === 'LI' && item.classList.contains('tabs__item--active') && tabsContent[idx].classList.contains('tabs__content--active')) {
+      item.classList.remove('tabs__item--active');
+      tabsContent[idx].classList.remove('tabs__content--active');
+    }
+  });
+}
+
+function isChildOfHeader(node) {
+  return node.parentNode.classList.contains('accordion__header');
+}
+
+function isElementImage(element) {
+  return element != null && element.classList.contains('accordion__image');
+}
+
+function isBrotherOfElementImage(node) {
+  return node.nextSibling != undefined && isElementImage(node.nextSibling);
+}
+
+function findElementImage(listNodes) {
+  var elementImg = Array.from(listNodes).filter(function (node) {
+    return isBrotherOfElementImage(node);
+  });
+  return elementImg[0].nextSibling;
+}
+
+function addClassActivetoImgElement(node) {
+  var parentNode = null;
+
+  if (isChildOfHeader(node)) {
+    parentNode = node.parentNode;
+  } else {
+    parentNode = node;
+  }
+
+  var elementImg = findElementImage(parentNode.childNodes);
+  if (elementImg === null) return;
+  elementImg.classList.toggle('accordion__image--active');
+}
+
+function addClassActivetoItemElement(node) {
+  var item = null;
+
+  if (isChildOfHeader(node)) {
+    item = node.parentNode.parentNode;
+  } else {
+    item = node.parentNode;
+  }
+
+  item.classList.toggle('accordion__item--active');
+}
+/* EVENTS */
+
+
 iconMenu.addEventListener('click', function (evt) {
   if (mainNav.classList.contains('main-nav--show')) {
     iconMenu.src = '/assets/images/icon-hamburger.svg';
@@ -36,17 +103,11 @@ tabs.addEventListener('click', function (evt) {
   removeClassItems(tabsItem);
   addClassActive(evt.target, idx);
 });
+accordion.addEventListener('click', function (evt) {
+  var targetClass = evt.target.classList;
 
-function addClassActive(element, idx) {
-  if (element.nodeName === 'LI') element.classList.add('tabs__item--active');
-  tabsContent[idx].classList.add('tabs__content--active');
-}
-
-function removeClassItems(items) {
-  items.forEach(function (item, idx) {
-    if (item.nodeName === 'LI' && item.classList.contains('tabs__item--active') && tabsContent[idx].classList.contains('tabs__content--active')) {
-      item.classList.remove('tabs__item--active');
-      tabsContent[idx].classList.remove('tabs__content--active');
-    }
-  });
-}
+  if (targetClass.contains('accordion__title') || targetClass.contains('accordion__header') || targetClass.contains('accordion__image')) {
+    addClassActivetoItemElement(evt.target);
+    addClassActivetoImgElement(evt.target);
+  }
+});
--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,60 @@ const tabs = document.querySelector('#tabs')
 const tabsItem = [...document.querySelectorAll('.tabs__item')]
 const tabsContent = [...document.querySelectorAll('.tabs__content')]
 
+const accordion = document.querySelector('#accordion')
+const accordionItems = [...document.querySelectorAll('.accordion__item')]
+
+/* FUNCTIONS */
+
+function addClassActive(element, idx){
+    if(element.nodeName === 'LI') element.classList.add('tabs__item--active')
+    tabsContent[idx].classList.add('tabs__content--active')
+}
+function removeClassItems(items){
+    items.forEach((item, idx)=> {
+        if(item.nodeName === 'LI' && item.classList.contains('tabs__item--active') 
+            && tabsContent[idx].classList.contains('tabs__content--active')){
+            item.classList.remove('tabs__item--active')
+            tabsContent[idx].classList.remove('tabs__content--active')
+        }
+    });
+}
+function isChildOfHeader(node){
+    return node.parentNode.classList.contains('accordion__header')
+}
+function isElementImage(element){
+    return element != null && element.classList.contains('accordion__image')
+}
+function isBrotherOfElementImage(node){
+    return node.nextSibling != undefined && isElementImage(node.nextSibling)
+}
+function findElementImage(listNodes){
+    const elementImg = Array.from(listNodes).filter(node => isBrotherOfElementImage(node))
+    
+    return elementImg[0].nextSibling
+}
+function addClassActivetoImgElement(node){
+    let parentNode = null
+    if(isChildOfHeader(node)){
+        parentNode = node.parentNode
+    }else{
+        parentNode = node
+    }
+    const elementImg = findElementImage(parentNode.childNodes)
+    if(elementImg === null) return 
+    elementImg.classList.toggle('accordion__image--active')
+}
+function addClassActivetoItemElement(node){
+    let item = null
+    if (isChildOfHeader(node)){
+        item = node.parentNode.parentNode
+    }else{
+        item = node.parentNode
+    }
+    item.classList.toggle('accordion__item--active')
+}
+
+/* EVENTS */
 iconMenu.addEventListener('click', evt =>{
     if(mainNav.classList.contains('main-nav--show')){
         iconMenu.src = '/assets/images/icon-hamburger.svg'      
@@ -21,16 +75,13 @@ tabs.addEventListener('click', evt =>{
     removeClassItems(tabsItem)
     addClassActive(evt.target, idx)
 })
-function addClassActive(element, idx){
-    if(element.nodeName === 'LI') element.classList.add('tabs__item--active')
-    tabsContent[idx].classList.add('tabs__content--active')
-}
-function removeClassItems(items){
-    items.forEach((item, idx)=> {
-        if(item.nodeName === 'LI' && item.classList.contains('tabs__item--active') 
-            && tabsContent[idx].classList.contains('tabs__content--active')){
-            item.classList.remove('tabs__item--active')
-            tabsContent[idx].classList.remove('tabs__content--active')
-        }
-    });
-}
+
+accordion.addEventListener('click', evt =>{
+    const targetClass = evt.target.classList
+    if(targetClass.contains('accordion__title') || 
+        targetClass.contains('accordion__header') ||
+        targetClass.contains('accordion__image')){
+        addClassActivetoItemElement(evt.target)
+        addClassActivetoImgElement(evt.target)
+    }
+})
